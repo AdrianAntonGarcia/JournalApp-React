02ef56c0d791eb517e2955d67dad7f832875fbad
adriton <adrtler@gmail.com>
2020-07-24 17:05:25 +0200

    Estilos y estructura iniciales listos

--- src/components/journal/JournalScreen.js
+++ src/components/journal/JournalScreen.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Sidebar } from './Sidebar';
+import { NoteScreen } from '../notes/NoteScreen';
+// import { NothingSelected } from './NothingSelected';
 
 export const JournalScreen = () => {
   return (
     <div className="journal__main-content">
       <Sidebar />
-
       <main>
-        <h1>Main Cointet</h1>
+        {/* <NothingSelected /> */}
+        <NoteScreen />
       </main>
     </div>
   );
