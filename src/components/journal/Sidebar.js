dc0215906c9db90296770bfa2345d1985934aaf5
adriton <adrtler@gmail.com>
2020-07-28 19:03:20 +0200

    Nombre en el sidebar

--- src/components/journal/Sidebar.js
+++ src/components/journal/Sidebar.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { JournalEntries } from './JournalEntries';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { startLogout } from '../../actions/auth';
 
 export const Sidebar = () => {
+  const { name } = useSelector((state) => {
+    return state.auth;
+  });
   const dispatch = useDispatch();
   const handleLogout = () => {
     dispatch(startLogout());
@@ -13,7 +16,7 @@ export const Sidebar = () => {
       <div className="journal__sidebar-navbar">
         <h3 className="mt-5">
           <i className="far fa-moon"></i>
-          <span> Adrian</span>
+          <span> {name}</span>
         </h3>
         <button className="btn" onClick={handleLogout}>
           Logout
